89ea745fee1b04e5d3e2e5247ba7192aaf0f2ccf
bettyzhuangg <87810403+bettyzhuangg@users.noreply.github.com>
2021-07-27 13:06:25 +1000

    Delete Week2.js

--- Week 2/Week2.js
+++
@@ -1,7 +0,0 @@
-function NewsletterChoice(val) {
-    var element=document.getElementById('txtOther');
-    if(val=='Others')
-      element.style.display='block';
-    else  
-      element.style.display='none';
-}
