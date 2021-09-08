212306d8b6c06703b7a580fce7f7157b88811589
bettyzhuangg <87810403+bettyzhuangg@users.noreply.github.com>
2021-09-08 15:51:27 +1000

    Update main.js

--- Week 3/Task 2/main.js
+++ Week 3/Task 2/main.js
@@ -1,5 +1,6 @@
 // guess the number game
 function guesstheNumber() {
+//     generates random number from 0 to 100
     let num = Math.floor(Math.random() * 100);
 
     let guess;
@@ -7,7 +8,9 @@ function guesstheNumber() {
     
     do {
         guesscount++;
+//         prompt user to enter a number
         guess = prompt("Please guess a number between 0 to 100");
+//        if statements which displays different output based on user's input
         if (guess < num) {
         alert("Number is bigger than " + guess);
         }
@@ -18,11 +21,13 @@ function guesstheNumber() {
             alert("Yay! you got it");
         }
     } while (guess != num && guess != null);
+//     display the counter onto the screen
     let text = document.querySelector("#text").innerHTML = "<br>" + "You have guessed " + guesscount + " times!";
 }
 
 // prompts user to enter two numbers and display the result from the multiplication
 function multiply() {
+//     prompts user to enter 2 numbers
     num1 = prompt("Please enter the first number");
     num2 = prompt("Please enter the second number");
     document.getElementById("text").innerHTML += '<br>' + 'The multiplication of ' + num1 + " and " + num2 + " resulted in " + (num1*num2).toString();
@@ -31,6 +36,7 @@ function multiply() {
 
 // joins all element array into a string, seperate with a comma
 function join() {
+//     initialize the array
     var names = ['Betty', 'Janice', 'Tiffany'];
     document.getElementById("message").innerHTML += names.join(',');
 }
