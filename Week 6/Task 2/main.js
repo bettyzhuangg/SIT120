bcb20281a700dcec93bbbf37c75eaef64247c921
bettyzhuangg <87810403+bettyzhuangg@users.noreply.github.com>
2021-09-10 15:38:17 +1000

    Create main.js

---
+++ Week 6/Task 2/main.js
@@ -0,0 +1,8 @@
+var app = new Vue({
+    el: "#app",
+    data: {
+      title: 'What vegetables would you like to add to your sandwich?',
+      // an array which will then store all the options that user has checked
+      checkedVeg: []
+    }
+})
